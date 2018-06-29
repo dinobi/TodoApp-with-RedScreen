fdc5ba57f52ce89107aca216f45e2a5cb2c712ba
dinobi <dino.kennetcorp@gmail.com>
2018-06-29 15:14:39 +0100

    render views

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,21 @@
 class TodosController < RedScreen::BaseController
-  def create
-    ['Welcome home']
+
+  def index
+    { index: 'All todos' }
   end
+
+  def create
+    ['Create Todo']
+  end
+
+  def show
+    render :show, first_name: 'Dinobi'
+  end
+  
+  def new
+    render :new
+  end
+
+  def update;end
+
 end
